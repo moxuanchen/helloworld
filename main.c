f03b3d9f3d8ad935580b0dffb74e8c09df3ff515
Aaron Chen <aaron.nehc@gmail.com>
2018-01-19 16:37:41 +0800

    Update main.c
    Signed-off-by: Aaron Chen <aaron.nehc@gmail.com>

--- main.c
+++ main.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, Jenkins.\n");
+    printf("BUILD THIS PROJECT AUTOMATIC BY JENKINS.\n");
     return 0;
 }
 
