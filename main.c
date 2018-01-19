9e7b8c3980279369e65d060560f678676ad23ce4
Aaron Chen <aaron.nehc@gmail.com>
2018-01-19 16:01:12 +0800

    Add main function.
    Signed-off-by: Aaron Chen <aaron.nehc@gmail.com>

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, world.\n");
+    return 0;
+}
+
